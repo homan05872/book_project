33b26b21d130effab707fb0febefa1303d5b40fb
hoamn05872 <homan05872@gmail.com>
2023-05-04 14:57:38 +0900

    レビュー機能実装途中、プロフィール編集画面一旦保留

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import SignupView, profiel_new
+from .views import SignupView, profiel_new,profiel_edit, profiel_detail
 
 app_name = 'accounts'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),    
     path('signup/', SignupView.as_view(), name='signup'),
-    path('profiel/new/', profiel_new, name='profiel_new')
+    path('profiel/new/', profiel_new, name='profiel_new'),
+    path('profiel/detail/', profiel_detail, name='profiel_detail'),
+    path('profiel/edit/',profiel_edit,name='profiel_edit'),
 ]
