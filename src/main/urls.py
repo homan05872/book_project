c82d84697d9409cc4d2d16c4262505afcb6b968b
Ubuntu <ubuntu@ip-172-31-81-68.ec2.internal>
2023-07-19 17:45:54 +0900

    デプロイ後

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('staff-admin-homan/', admin.site.urls),
     path('accounts/', include('allauth.urls')), #これ追加
     path('app_accounts/', include('accounts.urls')),
     path('',include('book.urls')),
